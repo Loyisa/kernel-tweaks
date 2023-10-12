5ab6cf4e784b2347f89fb63d7e264ea4bd4abe78
Loyisa <loyisa893@gmail.com>
2023-10-13 02:37:13 +0800

    Add backup func to install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+backupFileName = "sysctl.conf-$(date +%Y-%m-%d-%H-%M-%S)"
 
-# Check if is root
-echo "Kernel Tweeks by Nfense."
-if [ "$EUID" -ne 0 ]
-  then echo "Please run this as root"
+echo "Kernel Tweeks by Nfense and DionaMC"
+
+# Check is we are root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please this script run as root."
   exit
 fi
+
+# Backup the /etc/sysctl.conf to current directory
+cp /etc/sysctl.conf ./$backupFileName
+echo "Backup sysctl.conf to ./$backupFileName"
 
 # Copy kernel settings to /etc
 cp ./sysctl.conf /etc
 
 # Reload kernel from file
 sysctl -p
-echo "Settings updated."
+echo "Kernel settings reloaded"
